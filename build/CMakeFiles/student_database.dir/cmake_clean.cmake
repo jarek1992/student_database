143d13e01c6760e32cdaad051ba77f5525cc2e83
Jarek Jeda <jarek.jeda@gmail.com>
2024-08-23 17:10:49 +0200

    update

---
+++ build/CMakeFiles/student_database.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/student_database.dir/source/dataBase.cpp.o"
+  "CMakeFiles/student_database.dir/source/dataBase.cpp.o.d"
+  "CMakeFiles/student_database.dir/source/main.cpp.o"
+  "CMakeFiles/student_database.dir/source/main.cpp.o.d"
+  "CMakeFiles/student_database.dir/source/peselValidator.cpp.o"
+  "CMakeFiles/student_database.dir/source/peselValidator.cpp.o.d"
+  "CMakeFiles/student_database.dir/source/student.cpp.o"
+  "CMakeFiles/student_database.dir/source/student.cpp.o.d"
+  "student_database"
+  "student_database.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/student_database.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
